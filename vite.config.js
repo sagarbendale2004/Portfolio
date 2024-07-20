9c44b9a61add38fc659efa757d224b9f5baa927f
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2024-07-20 21:53:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { resolve } from "path";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-export default {
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
-};
+});
+
