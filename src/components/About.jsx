da2758dde0115b0f82c6bd21d0a178f213ccacc7
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2025-01-15 15:19:48 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,27 +12,31 @@ function About() {
       <div className="row">
         <div className="info">
           <h3>
-            <span> Name : </span> Bendale Sagar Rajesh
+            <span> Name: </span> Bendale Sagar Rajesh
           </h3>
           <h3>
-            <span> Age : </span> 21{" "}
+            <span> Age: </span> 21
           </h3>
           <h3>
-            <span> Qualification : </span> Integrated-MCA (Fourth Year){" "}
+            <span> Qualification: </span> Integrated-MCA (Fourth Year)
           </h3>
           <h3>
-            <span> Hobbies : </span> Playing Chess, Reading Books, Learning New
-            Technologies, Traveling{" "}
+            <span> Hobbies: </span> Playing Chess, Reading Books, Learning New
+            Technologies, Traveling
           </h3>
           <h3>
-            <span> Technical Skills : </span> React, Javascript, Html, Css, Chakra-ui,
-            Scss. <br/>
-            <span> Basic Understandings - (Nodejs, Express, MongoDb) <span/>{" "}
+            <span> Technical Skills: </span> React, JavaScript, HTML, CSS, Chakra-UI,
+            SCSS. <br />
+            <span> Basic Understandings: </span> Node.js, Express, MongoDB
           </h3>
           <h3>
-            <span> Language : </span> English,Hindi,Marathi{" "}
+            <span> Languages: </span> English, Hindi, Marathi
           </h3>
-          <a href="https://drive.google.com/file/d/1e1Zfio2XXjQkNfPwp3XybGIzi_soobUz/view" target="_blank>
+          <a
+            href="https://drive.google.com/file/d/1e1Zfio2XXjQkNfPwp3XybGIzi_soobUz/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button className="btn">
               Download CV <i className="fas fa-download"></i>
             </button>
